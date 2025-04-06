f30fb5f82fae0b4e8f236548ec3fe136966eefd1
farhanaakbar <farhanaakbar10@gmail.com>
2025-04-06 23:14:00 +0600

    initial commit

--- .idea/libraries/liveTest01.dart
+++ .idea/libraries/liveTest01.dart
@@ -5,4 +5,14 @@ void main(){
     {"name": "Bob", "scores": [88, 76, 95]},
     {"name": "Charlie", "scores": [90, 92, 85]},
   ];
+  Map<String, double> avgScores = {};
+
+  for (var student in students) {
+    String name = student['name'];
+    List<int> scores = List<int>.from(student['scores']);
+    double avg = scores.reduce((a, b) => a + b) / scores.length;
+
+    // Round to 2 decimal places
+    avgScores[name] = double.parse(avg.toStringAsFixed(2));
+  }
 }
